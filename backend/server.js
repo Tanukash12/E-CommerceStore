8e8094198fd435a0638807672d64cee91d5fef2f
Tanushka12 <kashyaptanushka14@gmail.com>
2026-02-12 15:05:32 +0530

    chore: initialize package.json with basic project setup and dependencies

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.get("/products", (req, res) => {
+    res.send("Server is running! ready123");
+})
+
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+    connectDB();
+    console.log('Server is running on port 5000');
+})
+
+export default app;
+
